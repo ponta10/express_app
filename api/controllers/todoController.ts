f345262ff8e39b97bf2d96c68f0697a23d5e0a54
ponta10 <mponta2002@gmail.com>
2023-06-25 19:00:22 +0900

    add service layer

--- api/controllers/todoController.ts
+++ api/controllers/todoController.ts
@@ -1,12 +1,26 @@
+// controllers/todoController.ts
+
 import { Request, Response } from 'express';
-import Todo, { ITodo } from '../models/todo';
+import { TodoService } from '../services/todoService';
+import { ITodo } from '../models/todo';
 
 export const createTodo = async (req: Request, res: Response) => {
-  // Create a new todo
+  try {
+    const todo: ITodo = req.body;
+    const newTodo = await TodoService.createTodo(todo);
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 export const getAllTodos = async (req: Request, res: Response) => {
-  // Retrieve all todos
+  try {
+    const todos = await TodoService.getAllTodos();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
-// Add other CRUD operations as needed...
+
