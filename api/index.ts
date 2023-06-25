8921fa80e12aebcf90aee849fd6a8f4ee0a7a4c3
ponta10 <mponta2002@gmail.com>
2023-06-25 22:32:41 +0900

    add index comment

--- api/index.ts
+++ api/index.ts
@@ -5,12 +5,18 @@ import todoRoutes from "./routes/todoRoutes";
 
 require('dotenv').config();
 
+// Expressフレームワークのインスタンスを作成しています。これがサーバーアプリケーションの中心となります。
 const app: Express = express();
 
 const PORT: string | number = process.env.PORT || 4000;
 
+// これにより、クロスオリジンリソース共有（CORS）が有効になり、異なるオリジン（ドメイン、プロトコル、またはポート）からのリクエストをサーバーが受け付けるようになります。
 app.use(cors());
+
+// これにより、サーバーはJSON形式のHTTPリクエストボディを自動的に解析できるようになります。
 app.use(express.json());
+
+// このモジュールは、特定のパス（この場合はおそらくtodoに関連するパス）に対するリクエストをどのように処理するかを定義します。
 app.use(todoRoutes);
 
 const uri: string = `mongodb+srv://ponta:${process.env.MONGO_PASSWORD}@cluster0.tbbrjsp.mongodb.net/todo_db?retryWrites=true&w=majority`;
